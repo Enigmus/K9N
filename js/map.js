58742b845deaa63d8779b37814f99c846d9c85ba
sd <sd_net@mail.ru>
2022-02-14 23:31:47 +0300

    add map

--- js/map.js
+++ js/map.js
@@ -1,18 +1,18 @@
-ymaps.ready(init);
-
 function init() {
-    var myMap = new ymaps.Map("map", {
-            center: [55.760363, 37.621166],
-            zoom: 17
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
 
-    myMap.geoObjects               
-        .add(new ymaps.Placemark([55.760363, 37.621166], {
-            balloonContent: '',
-            iconCaption: ''
-        }, {
-            preset: 'islands#greenDotIconWithCaption'
-        }));
+    let map = new ymaps.Map('map',{
+        center: [55.760363, 37.621166],
+        zoom: 17
+    });
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 }
+
+ymaps.ready(init);
